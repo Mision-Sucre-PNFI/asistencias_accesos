d05021ea9bf9a1ac9ff41100290d34fe8ac741b1
Jasmin Parra <jasparra34@gmail.com>
2020-06-22 02:23:11 -0430

    COmentarios en Validar.php

--- controllers/validar.php
+++ controllers/validar.php
@@ -9,15 +9,18 @@ $clave = $_POST['clave'];
 
 $consultas = new consulta;
 
+// Reviso si el usuario existe
 $usuario_existe = $consultas->validar_usuario($nombre_usuario);
 
 if ($usuario_existe!="0")
 {
+    // El usuario existe, valido la contraseña
     echo "El usuario existe";
     $clave_valida = $consultas->validad_clave($nombre_usuario, $clave);
 
     if ($clave_valida!="0")
     {
+        //La clave es correcta, envio al usuario a la pagina de la app
         header("location:../principal.php");
         $_SESSION['nombre_usuario'] = strtoupper($nombre_usuario);
     }
